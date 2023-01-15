6eb55c78651d704a6915968e2019e46a24cad59d
shenkns <shenkns@gmail.com>
2023-01-15 13:53:40 +0300

    Server Target

--- RunServer.bat
+++ RunServer.bat
@@ -2,4 +2,4 @@
 set /P port="Enter server port: "
 if "%port%"=="" set port="8888"
 cd /d
-Start "" "%cd%\Binaries\Windows\MobileRobotServer.exe" -PixelStreamingIP=127.0.0.1 -PixelStreamingPort="%port%"
+Start "" "%cd%\Binaries\WindowsServer\MobileRobotServerServer.exe" -PixelStreamingIP=127.0.0.1 -PixelStreamingPort="%port%" -log
